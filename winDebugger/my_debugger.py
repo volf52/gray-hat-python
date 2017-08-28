e32ef336d1a7917c4b836fa7f501fdac6139f9a4
Muhammad Arslan <rslnkrmt2552@gmail.com>
2017-08-28 22:00:43 +0500

    Testing if debugger attached to process.

--- winDebugger/my_debugger.py
+++ winDebugger/my_debugger.py
@@ -11,7 +11,9 @@ kernel32 = windll.kernel32
 
 class debugger():
     def __init__(self):
-        pass
+        self.h_process = None
+        self.pid= None
+        self.debugger_active = False
     
     def load(self, path_to_exe):
         
@@ -38,7 +40,50 @@ class debugger():
                                    byref(process_information)):
             print "[*] We have successfully launched the process!"
             print "[*] PID : %d" % process_information.dwProcessId
+            
+            self.h_process = self.open_process(process_information.dwProcessId)
         
         else:
             print "[*] Error: 0x%08x." % kernel32.GetLastError()
+        
+    def open_process(self, pid):
+        return kernel32.OpenProcess(PROCESS_ALL_ACCESS, pid, False)
+    
+    def attach(self, pid):
+        
+        self.h_process = self.open_process(pid)
+        
+        if kernel32.DebugActiveProcess(pid):
+            self.debugger_active = True
+            self.pid = int(pid)
+            self.run()
+        else:
+            print "[*] Unable to attach to the process."
+    
+    def run(self):
+        
+        while self.debugger_active:
+            self.get_debug_event()
+    
+    def get_debug_event(self):
+        debug_event = DEBUG_EVENT()
+        continue_status = DBG_CONTINUE
+        
+        if kernel32.WaitForDebugEvent(byref(debug_event), INFINITE):
+            raw_input("Press a key to continue...")
+            self.debugger_active = False
+            kernel32.ContinueDebugEvent(debug_event.dwProcessId, debug_event.dwThreadId, continue_status)
+    
+    def detach(self):
+        
+        if kernel32.DebugActiveProcessStop(self.pid):
+            print "[*] Finished debugging. Exiting..."
+            return True
+        else:
+            print "There was an error."
+            return False
+    
+        
+             
+        
         
